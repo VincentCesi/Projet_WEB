f51db8950c48204eff0962fbabeed850bc148b7e
VincentCesi <vincent.bertin@viacesi.fr>
2018-04-10 15:33:55 +0200

    Je test

---
+++ Envoie image.php
@@ -0,0 +1,13 @@
+<html>
+   <head>
+      <title>Stock d'images</title>
+   </head>
+   <body>
+      <h3>Envoi d'une image</h3>
+      <form enctype="multipart/form-data" action="#" method="post">
+         <input type="hidden" name="MAX_FILE_SIZE" value="250000" />
+         <input type="file" name="fic" size=50 />
+         <input type="submit" value="Envoyer" />
+      </form>
+   </body>
+</html>
