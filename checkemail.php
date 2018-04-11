696de57a641d0bfd68ad7bfe2938f7451c6a21c8
Alpaga65 <dorian.marco@viacesi.fr>
2018-04-11 11:41:57 +0200

    Test MDP et Password

--- checkemail.php
+++ checkemail.php
@@ -6,13 +6,21 @@
     <script type="text/javascript">
 
         var re = /^[a-z0-9.-]+@[a-z0-9.-]{2,}.[a-z]{2,4}$/;
+        var re2 = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[\da-zA-Z]{8,16}$/;
 
-        function testInfo(email){
+        function testEmail(email){
             var OK = re.exec(email.value);
             if (!OK)
                 window.alert(email.input + " n'est pas une email correct!");
             else
                 window.alert("Email correct " + OK[0]);
+        }
+        function testPass(password){
+            var OK = re2.exec(password.value);
+            if (!OK)
+                window.alert(password.input + " n'est pas assez sécurisé!");
+            else
+                window.alert("Mot de passe convenable " + OK[0]);
         }
     </script>
     <link rel="stylesheet" href="style.css">
@@ -24,7 +32,10 @@
     <p>test pour email.
         <br>Le format attendu est xxxxxx@xxx.xx .</p>
     <form action="#">
-      <input id="email"><button onclick="testInfo(document.getElementById('email'));">Vérifier</button>
+        <input id="email"><button onclick="testEmail(document.getElementById('email'));">Vérifier</button>
+          <p>test pour password.
+        <br>Le format attendu est 1 majuscule 1 minuscules et 1 chiffres au mini .</p>
+        <input id="password"><button onclick="testPass(document.getElementById('password'));">Vérifier</button>
     </form>
   </body>
 </html>
