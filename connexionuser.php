92f135667b43fe2508d7d1a6a4f6f16458d7be8c
Alpaga65 <dorian.marco@viacesi.fr>
2018-04-11 09:08:03 +0200

    Pour elderion

--- connexionuser.php
+++ connexionuser.php
@@ -7,9 +7,7 @@ $hash = crypt($pass, $salt);
 bdd_inscription($pseudo, $hash); // Ici
 
 // Connexion
-$pass = 'mdp';
-$hash = bdd_get_hash($pseudo); // Le hash est enregistré lors de l'inscription (voir plus haut)
-if($hash == crypt($pass, $hash)) {
+password_verify();
     // Mot de passe correct
 }
 ?>
