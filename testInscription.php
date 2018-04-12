76ab76eaf661d10fb1baa54d9c7a287dcf8229a1
Alpaga65 <dorian.marco@viacesi.fr>
2018-04-12 10:39:04 +0200

    Juanito

--- testInscription.php
+++ testInscription.php
@@ -1,22 +1,27 @@
 <?php
 
-    $firstname = $_POST['firstname'];
+    $firstname = $_POST['firstName'];
     $name = $_POST['lastName'];
     $email = $_POST['email'];
     $password = $_POST['password'];
     $role = 0;
 
+    echo $firstname;
+    echo $name;
+    echo $email;
+    echo $password;
     $hash = password_hash( $password, PASSWORD_DEFAULT);
+    echo $hash;
 include ('connexiondb.php');
 
 // Requête préparée pour empêcher les injections SQL
-$requete = $bdd->prepare("INSERT INTO users (FirstName, Name, Email, Hash, ID_roles) VALUES( :firstname, :name, :email, :hash, ID_roles)");
+$requete = $bdd->prepare("INSERT INTO users (FirstName, Name, Email, Hash) VALUES( :firstname, :name, :email, :hash)");
 
     $requete->bindValue(':firstname', $firstname, PDO::PARAM_STR);
     $requete->bindValue(':name', $name, PDO::PARAM_STR);
     $requete->bindValue(':email', $email, PDO::PARAM_STR);
     $requete->bindValue(':hash', $hash, PDO::PARAM_STR);
-    $requete->bindValue(':role', $role, PDO::PARAM_STR);
+    //$requete->bindValue(':ID_roles', $role, PDO::PARAM_STR);
 
 $lecture = $bdd->prepare("SELECT Email FROM users");
 $lecture->execute();
@@ -28,9 +33,9 @@ while ($result = $lecture->fetch(PDO::FETCH_COLUMN))
     <p>
     <?php
  print_r($result); ?><br />
-        if ($pseudo != $result)
+        if ($email != $result)
         {
-        $requete->execute();
+        $lecture->execute();
         }else
             {
         <?php echo "Error système : l'email est déja utilisé" ?>
