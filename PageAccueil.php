e196178ac2120db313a8e9b553305626c88bc180
Elderion64 <antoine.barachet@viacesi.fr>
2018-04-13 11:48:31 +0200

    plein de chose j'ai la flemme de faire la liste

--- PageAccueil.php
+++ PageAccueil.php
@@ -1,104 +1,84 @@
+
 <!DOCTYPE htlm>
 <html>
 
     <head>
-
-        <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-        <!------ Include the above in your HEAD tag ---------->
-
         <meta charset="utf=8" />
         <title>Accueil</title>
 
-        <div id="bde">
-
-
-            <img src="images/logos/ExiaLogo.png" class="img" />
-
-            <div>
-                <h1>BDE Exia PAU</h1>
-                <input class="imagef" type="image" alt="facebook" src="images/logos/facebook-770688_960_720.png">
-                <input class="imaget" type="image" alt="twitter" src="images/logos/Twitter_Bird.svg.png">
-                <input class="imaged" type="image" alt="discord" src="images/logos/discord-new-logo.png">
-            </div>
-
-            <img src="images/logos/LogoBDExiaBlack_2.png" class="img" />
-        </div>
-
-
+        <?php
+            include("Pages/templates/head_bde.php");
+        ?>
     </head>
 
+
     <body>
-
         <div id="onglet">
+            <?php
+                include("Pages/templates/barre_nav.php");
+            ?>
 
-            <h3>ONGLET</h3>
+            <div class="txt_carousel">
+                <div class="txt">
+                    <?php
+                        print ("Bonjour et bienvenue dans notre site !!!! oui alors je saispas ce que je fous ici donc bon voila voila coucou maman je vais bien tout va bien je ne suis pas fou je suis juste différent donc ne t'en fait pas je vais bien tout va bien.oui alors je saispas ce que je fous ici donc bon voila voila coucou maman je vais bien tout va bien je ne suis pas fou je suis juste différent donc ne t'en fait pas je vais bien tout va bien.oui alors je saispas ce que je fous ici donc bon voila voila coucou maman je vais bien tout va bien je ne suis pas fou je suis juste différent donc ne t'en fait pas je vais bien tout va bien. :D");
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                        echo'<br>';
+                    ?>
+                </div>
 
+                <?php
+
+                    include("Pages/templates/carousel.php");
+
+                ?>
+            </div>
+
+            <div>
+                <?php
+                    include("Pages/templates/barre_coins.php");
+                ?>
+
+                <div class="bde">
+                    <?php
+                        print ("Voici les membres principaux de le team du BDE oui alors je sais pas ce que je fous ici donc bon voila voila coucou maman je vais bien tout va bien je ne suis pas fou je suis juste différent donc ne t'en fait pas je vais bien tout va bien.")
+                    ?>
+                </div>
+            </div>
         </div>
-
 
     </body>
 
     <footer>
 
-        <h3>Test bordure bas</h3>
-
+        <?php
+            include("Pages/templates/mention_footer.php");
+        ?>
     </footer>
 
 </html>
 
+<?php
 
-<style>
-
-    #bde{
-
-        display: flex;
-        justify-content: space-between;
-        margin-top: 20px;
-    }
-
-    #onglet{
-
-
-        justify-content: flex-start;
-        margin-top: 20px;
-    }
-
-    .img{
-
-        max-width: 20%;
-        height: 20%;
-    }
-
-    .imagef{
-
-        max-width: auto%;
-        height: 10%;
-    }
-
-
-    .imaget{
-
-        max-width: auto%;
-        height: 10%;
-    }
-
-    .imaged{
-
-        max-width: auto%;
-        height: 11%;
-    }
-
-    footer{
-
-
-
-        background: #999999;
-        padding: 1em;
-        margin-top: 20px;
-        text-align: center;
-
-    }
-
-</style>
+    include("Pages/templates/style.css");
+?>
---
+++ Pages/templates/barre_nav.php
@@ -0,0 +1,13 @@
+            <div class="nav">
+
+                <ul>
+                <li><a class="active" href="#accueil">Accueil</a></li>
+                <li><a href="#evenement">Evènements</a></li>
+                <li><a href="#boite_idee">Boîte à idée</a></li>
+                <li><a href="#association">Associations</a></li>
+                <li><a href="#bde">BDE</a></li>
+                <li><a href="#galeri">Galerie</a></li>
+
+            </ul>
+
+            </div>
