e95e2991b2ecc2bc2cf3c545b838b96f0524306e
Alpaga65 <dorian.marco@viacesi.fr>
2018-04-16 13:36:40 +0200

    Pas touch 2

--- Pages/templates/barre_nav.php
+++ Pages/templates/barre_nav.php
@@ -2,7 +2,7 @@
 
                 <ul>
                 <li><a class="active" href="index.php">Accueil</a></li>
-                <li><a href="#evenement">Evènements</a></li>
+                <li><a href="page_evenement_ok.php">Evènements</a></li>
                 <li><a href="page_evenement.php">Boîte à idée</a></li>
                 <li><a href="#association">Associations</a></li>
                 <li><a href="#bde">BDE</a></li>
