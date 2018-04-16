a06c4e503e48527d09155c9ab1ff631e63e88b20
Alpaga65 <dorian.marco@viacesi.fr>
2018-04-16 15:13:44 +0200

    Antoine

--- Pages/templates/barre_nav.php
+++ Pages/templates/barre_nav.php
@@ -3,8 +3,8 @@
 
                 <ul>
                 <li class="active"><a href="#accueil">Accueil</a></li>
-                <li><a href="#evenement">Evènements</a></li>
-                <li><a href="#boite_idee">Boîte à idée</a></li>
+                <li><a href="page_evenement_ok.php">Evènements</a></li>
+                <li><a href="page_evenement.php">Boîte à idée</a></li>
                 <li><a href="#association">Associations</a></li>
                 <li><a href="#bde">BDE</a></li>
                 <li><a href="#galerie">Galerie</a></li>
--- participationEvent.php
+++ participationEvent.php
@@ -1,14 +1,28 @@
 <?php
+$titre = $_POST['Titre'];
+$author = $_POST['nameAuthor'];
+$description = $_POST['description'];
+$dateStart = $_POST['dateStart'];
+$dateEnd = $_POST['dateEnd'];
+$image = $_POST['Image'];
+$dateAccept = $_POST['dateAccept'];
 
-session_start();
 
 include("connexiondb.php");
- $test = 4;
+ $Attendees = 1;
 
-$requeteParticipation = $bdd->prepare('INSERT INTO participate (ID, ID_EventValidated) VALUES(:id, :idEvent) ');
-        $requeteParticipation->bindValue(':id', $_SESSION['id'], PDO::PARAM_STR);
-        $requeteParticipation->bindValue(':idEvent', $test, PDO::PARAM_STR);
-$requeteParticipation->execute();
+$requeteParticipation = $bdd->prepare('INSERT INTO participate (Title, Author, Description, AcceptionDate,StartDate, EndDate, urlImage, Attendees)
+VALUES(:titre, :description, :dateAccept ,:dateStart, :dateEnd, :image)');
+        $requeteParticipation->bindValue(':titre', $titre, PDO::PARAM_STR);
+        $requeteParticipation->bindValue(':author', $author, PDO::PARAM_STR);
+        $requeteParticipation->bindValue(':description', $description, PDO::PARAM_STR);
+        $requeteParticipation->bindValue(':dateAccept', $dateAccept, PDO::PARAM_STR);
+        $requeteParticipation->bindValue(':dateStart', $dateStart, PDO::PARAM_STR);
+        $requeteParticipation->bindValue(':dateEnd',$dateEnd, PDO::PARAM_STR);
+        $requeteParticipation->bindValue(':image', $image, PDO::PARAM_STR);
+
+
+        $requeteParticipation->execute();
 
 header("Location: index.php");
 ?>
