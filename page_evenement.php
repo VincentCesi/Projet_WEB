55c3ec3d560339fe15ca7382aae1fc707f749e7f
Alpaga65 <dorian.marco@viacesi.fr>
2018-04-16 09:23:43 +0200

    Merci Juan

--- page_evenement.php
+++ page_evenement.php
@@ -3,14 +3,7 @@
 <html>
     <?php
     session_start();
-        if (isset($_SESSION['id'])) {
-
-            if ($_SESSION['id'] != 0) {
-
-
-
-            }
-        }else {
+        if (!isset($_SESSION['id'])) {
             header("Location: index.php");
         }
     ?>
