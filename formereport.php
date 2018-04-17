00f11a7f3d8ebaac9313b3fde0583ad4340c8f71
VincentCesi <vincent.bertin@viacesi.fr>
2018-04-17 13:35:22 +0200

    pas fini

---
+++ formereport.php
@@ -0,0 +1,17 @@
+<!DOCTYPE htlm>
+
+<html>
+   <head>
+      <title>Report</title>
+   </head>
+   <body>
+      <h3>Report</h3>
+       <form action="traitement.php" method="post">
+           <p>
+                Formulaire de report :<br />
+
+
+           </p>
+       </form>
+   </body>
+</html>
