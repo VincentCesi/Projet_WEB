11aea95b7b2c91628b2e3051619db31fd5fcbc44
Alpaga65 <dorian.marco@viacesi.fr>
2018-04-19 10:25:51 +0200

    Comment

--- envoiCommentaire.php
+++ envoiCommentaire.php
@@ -2,14 +2,15 @@
 
 
 $commentaire = $_POST['commentaire'];
+$idEvent = $_POST['id_evenement'];
 session_start();
 
 include('connexiondb.php');
 
-$requete = $bdd->prepare("INSERT INTO comments (Content, ID_User) VALUES( :commentaire, :user)");
+$requete = $bdd->prepare("INSERT INTO comments (Content, ID_User, ID_EventValidated) VALUES( :commentaire, :user, :idevent)");
 
 $requete->bindValue(':commentaire', $commentaire, PDO::PARAM_STR);
-
+$requete->bindValue(':idevent', $idEvent, PDO::PARAM_STR);
 $requete->bindValue(':user', $_SESSION['id'], PDO::PARAM_STR);
 $requete->execute();
 ?>
--- page_evenement_info.php
+++ page_evenement_info.php
@@ -109,6 +109,7 @@ include("Pages/templates/bootstrap.php");
                         </form>
                         <?php
                         include('connexiondb.php');
+                        $idEvent = $_POST['id_evenement'];
                         $requeteComment = $bdd->prepare('SELECT * FROM comments WHERE ID_EventValidated = :idEvent');
                         $requeteComment ->execute(array(':idEvent'=> $event['ID_EventValidated']));
                         while ($comment = $requeteComment->fetch())
