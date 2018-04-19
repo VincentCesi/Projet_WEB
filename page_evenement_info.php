ca08764a586d99c41d712ebeedfd3c42606e002b
Alpaga65 <dorian.marco@viacesi.fr>
2018-04-19 15:54:57 +0200

    Update

--- page_evenement_info.php
+++ page_evenement_info.php
@@ -112,7 +112,7 @@ include("Pages/templates/bootstrap.php");
 
                             include('connexiondb.php');
                             $idEvent = $_POST['id_evenement'];
-                            $requeteComment = $bdd->prepare('SELECT * FROM comments WHERE ID_EventValidated = :idEvent ORDER BY ID_Comment DESC');
+                            $requeteComment = $bdd->prepare('SELECT * FROM comments WHERE ID_EventValidated = :idEvent ORDER BY PostDate DESC ');
                             $requeteComment ->execute(array(':idEvent'=> $event['ID_EventValidated']));
                             while ($comment = $requeteComment->fetch())
                             {
