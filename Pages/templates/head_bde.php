5dabcd0af2d1d7b835c55a791104b638ebae08c5
Elderion64 <antoine.barachet@viacesi.fr>
2018-04-19 14:23:44 +0200

    gogogo

--- Pages/templates/head_bde.php
+++ Pages/templates/head_bde.php
@@ -1,14 +1,36 @@
-<div id="bde">
+<?php
+include("bootstrap.php");
+?>
 
-
+<div id="bde" class="col-md-12">
+    <div class="row">
+        <div class="col-sm-3">
             <img src="images/logos/ExiaLogo.png" class="img" />
 
-            <div>
+        </div>
+
+        <section class="col-sm-4 col-sm-offset-1">
+            <div class="row">
+            <div class="col-sm-12">
                 <h1>BDE Exia PAU</h1>
+
+            </div>
+
+            <div class="col-sm-12">
                 <input class="imagef" type="image" alt="facebook" src="images/logos/facebook-770688_960_720.png">
                 <input class="imaget" type="image" alt="twitter" src="images/logos/Twitter_Bird.svg.png">
                 <input class="imaged" type="image" alt="discord" src="images/logos/discord-new-logo.png">
             </div>
+            </div>
 
+
+        </section>
+
+        <div class="col-sm-3 col-sm-offset-1">
             <img src="images/logos/LogoBDExiaBlack_2.png" class="img" />
+
+        </div>
+
+    </div>
+
 </div>
