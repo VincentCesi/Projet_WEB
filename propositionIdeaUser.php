9bcee89689a616018a0ba0550a1138e8028aa6dd
Guerci jean <jeancharleg@gmail.com>
2018-04-19 08:30:36 +0200

    yolo

--- propositionIdeaUser.php
+++ propositionIdeaUser.php
@@ -3,7 +3,7 @@
 
    $title = $_POST['title'];
 
-   $nameAuthor = $_POST['nameAuthor'];
+   $nameAuthor = $_POST['Author'];
 
    $description = $_POST['description'];
     $event = $_POST['event'];
@@ -16,11 +16,11 @@
 
         // Requête préparée pour empêcher les injections SQL
     $requete = $bdd->prepare("INSERT INTO ideabox (Title, Description, Author, Validation, Event)
-    VALUES( :title, :nameAuthor, :description, :validated, :event)");
+    VALUES( :title, :Author, :description, :validated, :event)");
 
     $requete->bindValue(':title', $title, PDO::PARAM_STR);
     $requete->bindValue(':description', $validated, PDO::PARAM_STR);
-    $requete->bindValue(':nameAuthor', $nameAuthor, PDO::PARAM_STR);
+    $requete->bindValue(':Author', $nameAuthor, PDO::PARAM_STR);
     $requete->bindValue(':description', $description, PDO::PARAM_STR);
     $requete->bindValue(':validated', $validated, PDO::PARAM_STR);
     $requete->bindValue(':event', $event, PDO::PARAM_STR);
