c7f90e9d607ec10b88a71f00117ec239084a60bf
Alpaga65 <dorian.marco@viacesi.fr>
2018-04-19 13:38:44 +0200

    ewsxrdcfvgh

--- Pages/templates/barre_nav.php
+++ Pages/templates/barre_nav.php
@@ -8,6 +8,7 @@
             if ($_SESSION['role'] != 1) { ?>
         <li class="bar2"><a href="page_evenement.php">Evènements</a></li>
         <li class="bar3"><a href="page_boiteidee.php">Boîte à idée</a></li>
+        <li class="bar9"><a href="boutique.php">Boutique</a></li>
         <?php
             }}?>
         <li class="bar4"><a href="page_association.php">Associations</a></li>
