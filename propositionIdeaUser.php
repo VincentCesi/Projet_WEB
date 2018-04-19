a63c85397bcaa9fef9fd58866af12af564f138be
Alpaga65 <dorian.marco@viacesi.fr>
2018-04-19 15:07:07 +0200

    bljb

--- propositionIdeaUser.php
+++ propositionIdeaUser.php
@@ -1,23 +1,35 @@
 <?php
-header("Location: index.php");
+//header("Location: index.php");
+session_start();
 $title = $_POST['title'];
 $Author = $_POST['Author'];
 $description = $_POST['description'];
-$event = $_POST['event'];
+
+$validation = 0;
+if(!empty($_POST['event'])){
+    $event = $_POST['event'];
+
+}else{
+    $event = 0;
+
+}
 
 
 include("connexiondb.php");
 
 
-$requeteParticipation = $bdd->prepare('INSERT INTO ideabox (Title, Description, Author, Event )
-VALUES(:title, :description , :Author, :event)');
+$requeteParticipation = $bdd->prepare('INSERT INTO ideabox (Title, Description, Author, Validation, Event, ID_User)
+VALUES(:title, :description , :Author,:validation, :event, :iduser)');
 
-        $requeteParticipation->bindValue(':title', $titre, PDO::PARAM_STR);
-        $requeteParticipation->bindValue(':description', $description, PDO::PARAM_STR);
-        $requeteParticipation->bindValue(':Author', $Author, PDO::PARAM_STR);
-        $requeteParticipation->bindValue(':event', $event, PDO::PARAM_STR);
+$requeteParticipation->bindValue(':title', $title, PDO::PARAM_STR);
+$requeteParticipation->bindValue(':description', $description, PDO::PARAM_STR);
+$requeteParticipation->bindValue(':Author', $Author, PDO::PARAM_STR);
+$requeteParticipation->bindValue(':validation', $validation, PDO::PARAM_STR);
+$requeteParticipation->bindValue(':event', $event, PDO::PARAM_STR);
+$requeteParticipation->bindValue(':iduser', $_SESSION['id'], PDO::PARAM_STR);
 
 
-        $requeteParticipation->execute();
+
+$requeteParticipation->execute();
 
 ?>
